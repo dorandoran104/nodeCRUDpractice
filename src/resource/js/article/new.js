76976f3e6949b2fd60897087ceaf0d3539512940
dorandoran104 <pgh_96@naver.com>
2023-06-23 17:25:58 +0900

    image upload 구현중(이미지 올리면 자바스크립트로 미리보기)

--- src/resource/js/article/new.js
+++ src/resource/js/article/new.js
@@ -1,11 +1,39 @@
-const button = document.querySelector("#new_button")
+const button = document.querySelector("#new_button");
+const img_input = document.querySelector("#file");
+const img_area = document.querySelector("#img_area")
 
+img_input.addEventListener("change",(event)=>{
+  const file = event.target.files;
+  
+  
+
+  for(let i = 0; i<file.length; i++){
+    const fileReader = new FileReader();
+    
+    fileReader.onload = (event)=>{
+      let str = "<img style='width : 300px; height : 300px' src='" + event.target.result + "'>"
+      img_area.insertAdjacentHTML("beforeend",str);
+    }
+    fileReader.readAsDataURL(file[i]);
+  }
+})
+
+
+//submit 누를때
 button.addEventListener("click",(event)=>{
   event.preventDefault();
 
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
+
+  if(title.length == 0 || body.length == 0){
+    alert("빈칸이 있습니다.");
+    return
+  }
+
   const param = {
-    title : document.querySelector("#title").value,
-    body : document.querySelector("#body").value
+    title : title,
+    body : body
   }
 
   fetch("new",{
